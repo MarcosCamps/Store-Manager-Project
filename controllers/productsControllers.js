c404879eb3fa23a66f437f9de7c8b58fa397abea
Marcos Campos <marxmyers@icloud.com>
2022-07-03 18:01:33 -0300

    Sending requirement 4

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -20,9 +20,22 @@ const getById = async (req, res) => {
 
 const addProduct = async (req, res) => {
   const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      message: '"name" is required',
+    });
+  }
+  if (name.length < 5) {
+    return res.status(422).json({
+      message: '"name" length must be at least 5 characters long',
+    });
+  }
   const product = await productsService.addProduct({ name });
-  console.log(product);
-  if (product) return res.status(201).send({ id: product.id, name });
+  if (product) {
+    return res.status(201).send({
+      id: product.id, name,
+    });
+  }
 };
 
 module.exports = { getAll, getById, addProduct };
--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -3,24 +3,23 @@ const { expect } = require('chai');
 const connection = require('../../../models/connection');
 const productsModels = require('../../../models/productsModels');
 
-const payloadProducts = [
-  {
-    "id": 1,
-    "name": "Martelo de Thor"
-  },
-  {
-    "id": 2,
-    "name": "Traje de encolhimento"
-  },
-  {
-    "id": 3,
-    "name": "Escudo do Capitão América"
-  }
-]
-
 describe('Testing something about products', () => {
   describe('Testing about getAll', () => {
     beforeEach(async () => {
+      const payloadProducts = [
+        {
+          "id": 1,
+          "name": "Martelo de Thor"
+        },
+        {
+          "id": 2,
+          "name": "Traje de encolhimento"
+        },
+        {
+          "id": 3,
+          "name": "Escudo do Capitão América"
+        }
+      ]
       sinon.stub(connection, 'execute').resolves(payloadProducts)
     });
     afterEach(async () => {
